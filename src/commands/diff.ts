60dac1edb10c94886ef95794baa67957a5db6b6b
Dale Baldwin <dale@atypical.net.au>
2021-04-26 22:19:36 +1000

    feat: testing stdout commands

--- src/commands/diff.ts
+++ src/commands/diff.ts
@@ -1,23 +1,32 @@
-import { Command, flags } from "@oclif/command";
+import { Command, flags } from '@oclif/command'
+import { exec } from 'child_process'
 
 export default class GitDiff extends Command {
-  static description =
-    "Git the git diff between this branch and the target branch";
+  static description = 'Git the git diff between this branch and the target branch'
 
-  static examples = [`$ test-packager`];
+  static examples = [`$ test-packager`]
 
   static flags = {
-    help: flags.help({ char: "h" }),
+    help: flags.help({ char: 'h' })
     // branch: flags.string({ char: "b", description: "target branch" }),
-  };
+  }
 
-  static args = [{ name: "branch" }];
+  static args = [{ name: 'branch' }, { name: 'target branch' }]
 
   async run() {
-    const { args, flags } = this.parse(GitDiff);
-
-    if (args) {
-      console.log(args);
-    }
+    const { args, flags } = this.parse(GitDiff)
+    console.log(args)
+    // git diff --dirstat=files,0 HEAD~1
+    exec(`git diff --name-only HEAD~1 | awk -F "/*[^/]*/*$" '{ print ($1 == "" ? "." : $1); }' | sort | uniq`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+    })
   }
 }
