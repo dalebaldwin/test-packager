d4288763354f0908677067e7bf4e28fb22e52fc3
Dale Baldwin <dale@atypical.net.au>
2021-04-27 21:30:45 +1000

    feat: adds some custom control over how paths are provided

--- src/commands/callback.ts
+++ src/commands/callback.ts
@@ -8,7 +8,9 @@ export default class Callback extends Command {
   static examples = ['$ test-packager branch']
 
   static flags = {
-    help: flags.help({ char: 'h' })
+    help: flags.help({ char: 'h' }),
+    comma: flags.boolean({ char: 'c', description: 'split folder paths by comma' }),
+    space: flags.boolean({ char: 's', description: 'split folder paths by space' })
   }
 
   static args = [
@@ -17,8 +19,17 @@ export default class Callback extends Command {
   ]
 
   async run() {
-    const { args } = this.parse(Callback)
+    const { args, flags } = this.parse(Callback)
     const diff = await diffOutput(args.branch)
-    await execCallback(diff, args.callback)
+    const fileListComma = diff.toString()
+    const fileListSpace = diff.join(' ')
+
+    if (flags.comma) {
+      await execCallback(fileListComma, args.callback)
+    } else if (flags.space) {
+      await execCallback(fileListSpace, args.callback)
+    } else {
+      await execCallback(diff, args.callback)
+    }
   }
 }
