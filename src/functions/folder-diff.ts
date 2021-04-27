21729d6bac222560a40803fcc3810224dcb66c38
Dale Baldwin <dale@atypical.net.au>
2021-04-27 23:09:31 +1000

    feat: fixing up root include and updated package

--- src/functions/folder-diff.ts
+++ src/functions/folder-diff.ts
@@ -5,10 +5,10 @@ const diffOutput = async (branch: string, root: boolean) => {
   const diff = await exec(`git diff ${branch} --name-only | awk -F "/*[^/]*/*$" '{ print ($1 == "" ? "." : $1); }' | sort | uniq`)
   const output = diff.stdout.split('\n')
   if (root) {
+    return output.filter(Boolean)
+  } else {
     const removeRoot = output.filter((item: string) => item !== '.')
     return removeRoot.filter(Boolean)
-  } else {
-    return output.filter(Boolean)
   }
 }
 
